4da34362b86158019a4fac9b0e397c0b94320ed1
Arkadiusz Jagodzinski <226330@student.pwr.edu.pl>
2020-04-04 19:45:31 +0200

    Add isspam endpoint

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## Isspam
 
 ```
-POST /api/isspam
+GET /api/isspam
 Content-Type: application/json
 body:
 {
@@ -20,4 +20,12 @@ body:
 {
   “spamPropability”: double od 0 do 1  
 }
+```
+
+Example
+```
+curl --header "Content-Type: application/json" \
+  --request GET \
+  --data '{"content":"to jest spam"}' \
+  http://localhost:8000/isspam/
 ```
---
+++ spamchecker/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+import json
+import random
+from django.http import JsonResponse
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(requests):
+
+	if(requests.method != 'GET'):
+		return HttpResponse("Bad method", status=400)
+
+	if(requests.content_type != 'application/json'):
+		return HttpResponse("Wrong content type", status=400)
+
+	if(len(requests.body) == 0): 
+		return HttpResponse("Empty body", status=400)
+
+	data = json.loads(requests.body)
+
+	if not 'content' in data:
+		return HttpResponse("Content not found", status=400)
+
+	content = data['content']
+
+	if(len(content) > 1000):
+		return HttpResponse("Message too long", status=400)
+		
+	if(len(content) == 0):
+		return HttpResponse("Message too short", status=400)
+
+	return JsonResponse({'spamPropability': isSpam(content)})
+
+
+def isSpam(text) :
+	return round(random.random(), 2)
