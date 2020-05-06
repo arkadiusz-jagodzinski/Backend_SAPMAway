f9af518c8d514a80a21209bb7c8066741ee5a592
Arkadiusz Jagodzinski <226330@student.pwr.edu.pl>
2020-05-06 23:14:08 +0200

    Create simple sms model

--- spamchecker/models.py
+++ spamchecker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Sms(models.Model):
+    content = models.CharField(max_length=200)
+    spamProbability = models.FloatField()
+
+    def __str__(self):
+        return self.question_text
