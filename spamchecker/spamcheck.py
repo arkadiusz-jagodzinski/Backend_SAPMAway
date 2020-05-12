551b4f1f198af17ed780f13ce057ebad4fc0402d
LordSkin <bartek.kramnik@wp.pl>
2020-05-12 23:39:59 +0200

    funkcja generująca chmurę słów spamu

--- spamchecker/spamcheck.py
+++ spamchecker/spamcheck.py
@@ -1,10 +1,12 @@
 import os
 
+import matplotlib.pyplot as plt
 from joblib import dump
 from joblib import load
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
+from wordcloud import WordCloud
 
 clf = load(os.getcwd() + "/res/mlp_clf")
 
@@ -24,4 +26,18 @@ def retrain_and_save_model(spam_data):
                         MLPClassifier(activation='logistic', hidden_layer_sizes=10, learning_rate='constant',
                                       solver='adam'))
     clf.fit(spam_data[0, :], spam_data[1, :])
-    dump(clf, os.getcwd() + '/res/mlp_clf')
+    dump(clf, os.getcwd() + os.path.sep + 'res' + os.path.sep + 'mlp_clf')
+
+
+def generate_word_cloud():
+    """
+    Funkcja generuje, zwraca i zapisuje do pliku /res/cloud.png chumrę głównych słów spamu.
+    :return: obraz z chmurą słów spamu
+    """
+    wc = WordCloud(background_color="white", width=1000, height=1000, normalize_plurals=True).generate_from_frequencies(
+        clf[0].vocabulary_)
+    fig = plt.imshow(wc)
+    fig.axes.get_xaxis().set_visible(False)
+    fig.axes.get_yaxis().set_visible(False)
+    plt.savefig(os.path.sep + 'res' + os.path.sep + 'cloud.png', bbox_inches='tight')
+    return fig
